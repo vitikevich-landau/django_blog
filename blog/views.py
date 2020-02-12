2be8b83141c5b1c795e36151db1486b0f0829dad
Viktor <vitikevich@gmail.com>
2020-02-12 21:23:13 +0700

    end lesson 6

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import get_object_or_404
 
 from blog.models import Post, Tag
+
+
+class ObjectDetailMixin:
+    model = None
+    template = None
+
+    def get(self, request, slug):
+        obj = get_object_or_404(self.model, slug__iexact=slug)
+        return render(request, self.template, {self.model.__name__.lower(): obj})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/details.html'
 
 
 def posts_lists(request):
@@ -8,16 +24,11 @@ def posts_lists(request):
     return render(request, 'blog/index.html', {'posts': posts})
 
 
-def post_details(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/details.html', {'post': post})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
 
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', {'tags': tags})
-
-
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', {'tag': tag})
